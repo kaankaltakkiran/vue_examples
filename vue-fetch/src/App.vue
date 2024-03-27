d7d3d1f9f63fca6b70a982eb74d24acb0d17dbae
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-27 11:23:34 +0300

    vue project created and list products with fetch and fake api

---
+++ vue-fetch/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<list-products></list-products>
+</template>
+
+<script>
+import ListProducts from "./components/ListProducts.vue";
+export default{
+  components:{
+    "list-products": ListProducts
+  }
+}
+</script>
+<style scoped>
+
+</style>
