fddee73e09b1667acf8238ba0624c7fd5ed6031b
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-28 11:46:10 +0300

    how store access app vue and other component

--- vue-pinia-egitim/src/App.vue
+++ vue-pinia-egitim/src/App.vue
@@ -1,14 +1,36 @@
 <template>
     <header>
       <img src="@/assets/pinia-logo.svg" alt="Pinia Loog" title="Pinia Logo">
-      <h1>Pinia Tasks</h1>
+      <h1>My Daily</h1>
     </header>
+    <main>
+     <div class="container">
+    <!--   vfor ile storedan gelen günlük listesini dönüyoruz. -->
+      <div class="gunluk-listesi" v-for="diary in diaryStore.diary">
+        <!-- vbind yaptın isim ile compenenteki props ismi aynı olmalı -->
+        <diary-details v-bind:diarylist="diary" :key="diary.id"></diary-details>
+      </div>
+     </div>
+    </main>
 </template> 
 
 <script>
+/* stores oluşturduğumuz yapıyı import ediyoruz. */
+import {useDiaryStore} from './stores/DiaryStore'
+/* günlük detay için import */
+import DiaryDetails from './components/DiaryDetails.vue'
   export default {
-    
+  components: {
+    "diary-details": DiaryDetails
+  },
+    setup(){
+    /*   import ettiğimiz storu kullanmak için değişken oluşturuyoruz. */
+      const diaryStore = useDiaryStore();
+      return {
+        diaryStore
+    }
   }
+}
 </script>
 
 <style scoped>
---
+++ vue-pinia-egitim/src/components/DiaryDetails.vue
@@ -0,0 +1,19 @@
+<template>
+         <div class="box">
+          <div>
+            <p>{{ diarylist.title }}</p>
+            <small class="date">{{diarylist.date}}</small>
+          </div>
+          <div>
+            <i class="material-icons">favorite</i>
+            <i class="material-icons">delete</i>
+          </div>
+        </div>
+</template>
+
+<script>
+  export default {
+ /*    ana bileşenden gelen propsu alıyoruz. */
+    props: ['diarylist']
+  }
+</script>
