539d726a1d5466e40be3423ef0acc91f1b37b7cb
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-28 16:38:12 +0300

    reset state and storeToRefs

--- vue-pinia-egitim/src/App.vue
+++ vue-pinia-egitim/src/App.vue
@@ -7,8 +7,8 @@
      <div class="container">
      <new-diary></new-diary>
       <div class="data-area">
-        <p v-if="filter === 'all'">Toplam {{ diaryStore.totalCount }} kayıt var</p>
-        <p v-if="filter === 'favs'">Toplam {{ diaryStore.favCount }} kayıt var</p>
+        <p v-if="filter === 'all'">Toplam {{ totalCount }} kayıt var</p>
+        <p v-if="filter === 'favs'">Toplam {{ favCount }} kayıt var</p>
         <div>
          <!--  ilgili butona tıkladığımızda filter değişkenine değer atıyoruz. -->
           <button @click="filter='all'">Tüm Günlüklerim</button>
@@ -17,17 +17,19 @@
       </div>
     <!--   vfor ile storedan gelen günlük listesini dönüyoruz. -->
     <!-- vif ile filtere göre günlükleri listeliyoruz. -->
-      <div class="gunluk-listesi" v-for="diary in diaryStore.diary" v-if="filter==='all'">
+      <div class="gunluk-listesi" v-for="diary in diary" v-if="filter==='all'">
         <!-- vbind yaptın isim ile compenenteki props ismi aynı olmalı -->
         <diary-details v-bind:diarylist="diary" :key="diary.id"></diary-details>
       </div>
 
       <!--   vfor ile storedan gelen günlük listesini dönüyoruz. -->
       <!-- getters ile favori günlükleri alıyoruz. -->
-      <div class="gunluk-listesi" v-for="diary in diaryStore.favs" v-if="filter==='favs'">
+      <div class="gunluk-listesi" v-for="diary in favs" v-if="filter==='favs'">
         <!-- vbind yaptın isim ile compenenteki props ismi aynı olmalı -->
         <diary-details v-bind:diarylist="diary" :key="diary.id"></diary-details>
       </div>
+     <!--  ilk state drumunua getirmek için reset -->
+      <p><button @click="diaryStore.$reset">Reset</button></p>
      </div>
     </main>
 </template> 
@@ -41,6 +43,8 @@ import {useDiaryStore} from './stores/DiaryStore'
 import DiaryDetails from './components/DiaryDetails.vue'
 /* yeni günlük ekleme için import  */
 import NewDiary from './components/NewDiary.vue'
+/* storeToRefs import ediyoruz. */
+import { storeToRefs } from 'pinia';
   export default {
   components: {
     "diary-details": DiaryDetails,
@@ -49,13 +53,20 @@ import NewDiary from './components/NewDiary.vue'
     setup(){
     /*   import ettiğimiz storu kullanmak için değişken oluşturuyoruz. */
       const diaryStore = useDiaryStore();
+    /*   storeToRefs ile store değişkenlerini dışarıya çıkartıyoruz. */
+      const { diary, loading, favs, totalCount, favCount } = storeToRefs(diaryStore)
       const filter = ref('all');
    /*    json ile veriyi çağırma işlemi yapıyoruz.  */
       diaryStore.fetchDiary();
     /*   return ile değişkenleri dışarıya döndürüyoruz. */
       return {
         diaryStore,
-        filter
+        filter,
+        diary,
+        loading,
+        favs,
+        totalCount,
+        favCount
     }
   }
 }
