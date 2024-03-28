1874fa936188840d12e5324a34653f014064a8f8
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-28 11:00:17 +0300

    vue pinia lesson project created

---
+++ vue-pinia-egitim/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+      <img src="@/assets/pinia-logo.svg" alt="Pinia Loog" title="Pinia Logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+</template> 
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
