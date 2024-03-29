12890f76703d74e0421d6452af690a6c5384333f
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-29 14:14:32 +0300

    coding home page after login

--- vue-jwt-example/src/views/Home.vue
+++ vue-jwt-example/src/views/Home.vue
@@ -1,9 +1,28 @@
 <template>
-<h1>Home</h1>
+<h1>{{message}}</h1>
 </template>
 
-<script>
+<script lang="ts">
   export default {
     name: 'Home',
+    setup(){
+      
+      const message=ref('You are not logged in');
+      /*  sayfa yüklendiğinde çalışır. */
+     onMounted(async() => {
+       const response= await fetch('http://localhost:5173/api/user',{
+          headers:{
+            'Content-Type':'application/json'
+          },
+          credentials: 'include',
+        });
+      /*   gelen veriler */
+       /*  JSON formatından JavaScript nesnesine dönüştürülür. */
+        const content= await response.json();
+
+        message.value=`Welcome ${content.name}`;
+     });
+      return {message};
+    }
   }
 </script>
