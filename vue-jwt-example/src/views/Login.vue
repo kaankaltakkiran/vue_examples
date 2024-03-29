5ad899d87a20e9b03813a6e51016c4eb851dc47c
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-29 12:08:35 +0300

    template bug fixed

--- vue-jwt-example/src/views/Login.vue
+++ vue-jwt-example/src/views/Login.vue
@@ -1,14 +1,12 @@
 <template>
   <form>
-    <h1 class="h3 mb-3 fw-normal">Please register</h1>
-
-    <input  class="form-control" placeholder="Name" required>
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
     <input  type="email" class="form-control" placeholder="Email" required>
 
     <input  type="password" class="form-control" placeholder="Password" required>
 
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
   </form>
 </template>
 
--- vue-jwt-example/src/views/Register.vue
+++ vue-jwt-example/src/views/Register.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
+  <form>
+    <h1 class="h3 mb-3 fw-normal">Please register</h1>
 
-  </div>
+    <input  class="form-control" placeholder="Name" required>
+
+    <input  type="email" class="form-control" placeholder="Email" required>
+
+    <input  type="password" class="form-control" placeholder="Password" required>
+
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+  </form>
 </template>
 
 <script>
