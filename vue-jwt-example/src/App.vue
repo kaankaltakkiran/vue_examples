ffa96c62cd43f76f1020406fbd97f88d068c02de
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-29 12:05:50 +0300

    create template and navigation

---
+++ vue-jwt-example/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+<app-navbar></app-navbar>
+  <main class="form-signin">
+<router-view></router-view>
+</main>
+</template>
+
+<script>
+import NavbarVue from './components/Navbar.vue'
+export default {
+  components:{
+    "app-navbar": NavbarVue
+  }
+}
+</script>
+
+<style>
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+
+.form-signin .checkbox {
+  font-weight: 400;
+}
+
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
---
+++ vue-jwt-example/src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <form>
+    <h1 class="h3 mb-3 fw-normal">Please register</h1>
+
+    <input  class="form-control" placeholder="Name" required>
+
+    <input  type="email" class="form-control" placeholder="Email" required>
+
+    <input  type="password" class="form-control" placeholder="Password" required>
+
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+  }
+</script>
