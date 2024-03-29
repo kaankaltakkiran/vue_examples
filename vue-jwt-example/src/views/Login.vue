f028864ae985a4e5d9c8befeeb17df76b7324134
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-29 12:45:47 +0300

    login page created and router home page

--- vue-jwt-example/src/views/Login.vue
+++ vue-jwt-example/src/views/Login.vue
@@ -1,17 +1,44 @@
 <template>
-  <form>
+<!--   @submit.prevent : formun submit edilmesini engeller. -->
+  <form @submit.prevent="submit">
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-    <input  type="email" class="form-control" placeholder="Email" required>
+    <input v-model="data.email"  type="email" class="form-control" placeholder="Email" required>
 
-    <input  type="password" class="form-control" placeholder="Password" required>
+    <input v-model="data.password"  type="password" class="form-control" placeholder="Password" required>
 
     <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
   </form>
 </template>
 
-<script>
+<script lang="ts">
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
   export default {
     name: 'Login',
+    setup(){
+  /*     reactive:Herhangi bir yerde değerini değiştirdiğinizde kullandığınız her yerde anında değerinin değiştiğini gözlemleyebildiğiniz bir değişken. */
+      const data=reactive({
+        email:'',
+        password:'',
+      });
+      const router=useRouter();
+      const submit= async()=>{
+        await fetch('http://localhost:5173/api/login',{
+          method:'POST',
+          headers:{
+            'Content-Type':'application/json'
+          },
+       /*    seçeneğini kullanarak, tarayıcı otomatik olarak mevcut oturum çerezlerini isteklere ekleyecektir. Bu, sunucunun oturum bilgilerini almasını ve kullanıcıyı doğrulamasını sağlar. */
+          credentials: 'include',
+          /* JavaScript nesnesini bir JSON dizesine dönüştürür.  */
+          body:JSON.stringify(data)
+        });
+     /*    işlem başarılı ise home sayfasına yönlendirme yapılır. */
+        await router.push('/');
+        console.log(data);
+      }
+      return {data};
+    }
   }
 </script>
