e6226563542ee45e12ae4d0b90a93eabb70cb74c
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-04-03 14:03:54 +0300

    code upgrade vue 3 and fixed comment line

--- vue-ajax/src/components/ListProducts.vue
+++ vue-ajax/src/components/ListProducts.vue
@@ -20,10 +20,10 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="product in filtredProducts" :key="product.id">
+          <tr v-for="product in filteredProducts" :key="product.id">
             <th>{{ product.id }}</th>
             <td><img :src="product.image" :alt="product.title" width="100" height="100"></td>
-            <td><router-link :to="'/product/' + product.id">{{ product.title }}</router-link></td> 
+            <td>{{ product.title }}</td> 
             <td>{{ product.description }}</td>
             <td>{{ product.price }}</td>
             <td>{{ product.category }}</td>
@@ -34,50 +34,46 @@
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      products: [],
-      keyword: '',
-      loading: true,
-      errored: false 
-    }
-  },
-  computed: {
-    filtredProducts() {
-      return this.products.filter(product => {
-        return product.title.toLowerCase().includes(this.keyword.toLowerCase());
-      });
-    }
-  },
-  created() {
-/*     ajax çarğısı ile veri çekme */
-    const xhr = new XMLHttpRequest();
-  /*   XMLHttpRequest nesnesine ait open metodu AJAX isteği açmak için kullanılır. */
-/*   XMLHttpRequest.open(method, url, async, user, password); */
-    xhr.open('GET', 'https://fakestoreapi.com/products/', true);
-    xhr.onreadystatechange = () => {
-/*       İstek başlatılmadığında readyState özelliği 0, açıldığında 1, istek gönderildiğinde 2, yüklendiğinde 3 ve tamamlandığında 4 değerini alır.
-İşlemleri readyState değeri 4 ve HTTP durum kodunun 200 olması ile gerçekleştirmek olası hataları önleyecektir. */
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-        /*   İstek sonucunda dönen değerler response, responseText ve responseXML ile alınır. */
-          this.products = JSON.parse(xhr.responseText);
-          console.log(this.products);
-          this.loading = false;
-        } else {
-          console.error('Error fetching products:', xhr.statusText);
-          this.errored = true;
-          this.loading = false;
-        }
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+//! Başlangıç değerleri
+const products = ref([]);
+const keyword = ref('');
+const loading = ref(true);
+const errored = ref(false);
+//Computed fonksiyonu ile filtreleme işlemi
+const filteredProducts = computed(() => {
+  return products.value.filter(product => {
+    return product.title.toLowerCase().includes(keyword.value.toLowerCase());
+  });
+});
+//Veri çekme işlemi
+const fetchData = () => {
+  const xhr = new XMLHttpRequest(); // XMLHttpRequest nesnesine ait open metodu AJAX isteği açmak için kullanılır.
+ // XMLHttpRequest.open(method, url, async, user, password);  
+  xhr.open('GET', 'https://fakestoreapi.com/products/', true);
+  xhr.onreadystatechange = () => {
+    /* readyState=0 ise istek başlatılmadı.
+    readyState=1 ise istek açıldı.
+    readyState=2 ise istek gönderildi.
+    readyState=3 ise istek yüklendi.
+    readyState=4 ise istek tamamlandı.
+*/
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) { //HTTP durum kodunun 200 olması ile gerçekleştirmek olası hataları önleyecektir.
+        products.value = JSON.parse(xhr.responseText);
+        loading.value = false;
+      } else {
+        console.error('Error fetching products:', xhr.statusText);
+        errored.value = true;
+        loading.value = false;
       }
-    };
-  /*   ajax başlatma */
-    xhr.send();
-  }
-}
+    }
+  };
+  xhr.send();
+};
+//Sayfa yüklendiğinde veri çekme işlemi
+onMounted(() => {
+  fetchData();
+});
 </script>
-
-<style scoped>
-</style>
